bde8c36679d4ce96d1e1b11bcbb5bdd4063c71c2
Bubbet <brett1057228@gmail.com>
2022-06-11 19:52:53 -0600

    make player items use new class

--- Aerolt/Buttons/EditPlayerItemButton.cs
+++ Aerolt/Buttons/EditPlayerItemButton.cs
@@ -13,10 +13,7 @@ namespace Aerolt.Buttons
         public GameObject buttonParent;
 
         public GameObject itemListParent;
-
-        public static GameObject _globalButtonPrefab;
-        public static GameObject _globalitemListParent;
-
+        
         private NetworkUser _user;
         
         public EditPlayerItemButton(NetworkUser user)
@@ -35,54 +32,8 @@ namespace Aerolt.Buttons
 
         private void Awake()
         {
-            _globalButtonPrefab = buttonPrefab;
-            _globalitemListParent = itemListParent;
-
             foreach (var def in ContentManager._itemDefs)
-                new ButtonStruct(def, buttonParent,false);
-        }
-    }
-    public class ButtonStruct
-    {
-        public ItemDef _def;
-        public GameObject _button;
-        public ButtonStruct CountButton;
-        public ButtonStruct(ItemDef def, GameObject ButtonParent, bool doDestroy = true)
-        {
-            _def = def;
-            _button = CreateButton(ButtonParent);
-            _button.GetComponent<Button>().onClick.AddListener(!doDestroy ? AddItemToList : DestroyButton);
-        }
-        public GameObject CreateButton(GameObject buttonParent)
-        {
-            var newButton = GameObject.Instantiate(EditPlayerItemButton._globalButtonPrefab, buttonParent.transform);
-            
-            newButton.GetComponent<CustomButton>().ButtonText.text = Language.GetString(_def.nameToken);
-            newButton.GetComponent<Image>().sprite = _def.pickupIconSprite;
-            return newButton;
-        }
-        public void DestroyButton()
-        {
-            LobbyManager.ItemDef[_def]--;
-            _button.GetComponent<CustomButton>().ButtonText.text = $"{Language.GetString(_def.nameToken)} x{LobbyManager.ItemDef[_def]}";
-
-            if (LobbyManager.ItemDef[_def] == 0)
-            {
-                LobbyManager.ItemDef.Remove(_def);
-                GameObject.Destroy(_button);
-            }
-            
-        }
-        public void AddItemToList()
-        {
-            if (!LobbyManager.ItemDef.ContainsKey(_def))
-            {
-                CountButton = new ButtonStruct(_def, EditPlayerItemButton._globalitemListParent);
-                LobbyManager.ItemDef.Add(_def, 0);
-            }
-            LobbyManager.ItemDef[_def]++;
-            CountButton._button.GetComponent<CustomButton>().ButtonText.text = $"{Language.GetString(_def.nameToken)} x{LobbyManager.ItemDef[_def]}";
-
+                new InventoryItemAddRemoveButtonGen(def, buttonPrefab, LobbyManager.ItemDef, itemListParent, buttonParent, false);
         }
     }
 }
