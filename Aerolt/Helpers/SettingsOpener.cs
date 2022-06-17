e6be656427a13bd34fcad315064971862a4151df
Bubbet <brett1057228@gmail.com>
2022-06-17 12:59:44 -0600

    SettingsOpener.cs behaviour

--- Aerolt/Helpers/SettingsOpener.cs
+++ Aerolt/Helpers/SettingsOpener.cs
@@ -1,13 +1,74 @@
+using System;
+using System.Linq;
+using Aerolt.Managers;
+using BepInEx.Bootstrap;
+using RiskOfOptions.Components.Panel;
+using RoR2;
+using RoR2.UI;
 using UnityEngine;
+using Console = RoR2.Console;
 
 namespace Aerolt.Helpers
 {
     public class SettingsOpener : MonoBehaviour
     {
+        private OpenState openSettings;
+        private LocalUser localuser;
+        private Transform headerTransform;
+
         public void OpenSettingsForced()
         {
-            
+            Console.instance.SubmitCmd(null, "pause", false); // This is how you do it, apparently splitscreen users do not have their own pause screen despite it being mostly implemented that way.
+            localuser = GetComponentInParent<PanelManager>().owner.localUser;
+            if (localuser != null)
+                openSettings = OpenState.WaitingForPause;
         }
-        
+
+        private void Update()
+        {
+            if (openSettings == OpenState.Idle) return;
+            var screen = PauseScreenController.instancesList.FirstOrDefault(x => x.eventSystemProvider.resolvedEventSystem == localuser?.eventSystem);
+            switch (openSettings)
+            {
+                case OpenState.WaitingForPause:
+                    if (screen && Chainloader.PluginInfos.ContainsKey("bubbet.zioriskofoptions"))
+                    {
+                        screen.OpenSettingsMenu();
+                        headerTransform = screen.submenuObject.transform.Find("SafeArea/HeaderContainer/Header (JUICED)");
+                        openSettings = OpenState.WaitingForRoO;
+                        return;
+                    }
+                    break;
+                case OpenState.WaitingForRoO:
+                    var modSettings = headerTransform.Find("GenericHeaderButton (Mod Options)");
+                    if (modSettings)
+                    {
+                        modSettings.GetComponent<HGButton>().InvokeClick();
+                        openSettings = OpenState.WaitingForCategory;
+                    }
+                    return;
+                case OpenState.WaitingForCategory:
+                    OpenSettings(screen);
+                    return;
+            }
+            openSettings = OpenState.Idle;
+        }
+
+        private void OpenSettings(PauseScreenController screen)
+        {
+            var category = screen.submenuObject.transform.Find(
+                $"SafeArea/SubPanelArea/SettingsSubPanel, (Mod Options)/Mod List Panel(Clone)/Scroll View/Viewport/VerticalLayout/ModListButton ({Load.Name + " " + localuser.currentNetworkUser.GetNetworkPlayerName().GetResolvedName()})");
+            if (!category) return;
+            category.GetComponent<ModListButton>().InvokeClick();
+            openSettings = OpenState.Idle;
+        }
+
+        private enum OpenState
+        {
+            Idle,
+            WaitingForPause,
+            WaitingForRoO,
+            WaitingForCategory
+        }
     }
 }
