a8678c33fe47d2624b5309281132b5d6012e8f5f
Bubbet <brett1057228@gmail.com>
2022-06-17 10:06:38 -0600

    fix some key already exist errors in ImageColorer.cs

--- Aerolt/Helpers/ImageColorer.cs
+++ Aerolt/Helpers/ImageColorer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Aerolt.Managers;
 using BepInEx.Bootstrap;
+using BepInEx.Configuration;
 using RiskOfOptions;
 using UnityEngine;
 using UnityEngine.UI;
@@ -19,30 +20,33 @@ namespace Aerolt.Helpers
         private ZioConfigFile.ZioConfigFile configFile;
         [NonSerialized] public Image image;
         [NonSerialized] public ZioConfigEntry<Color> configEntry;
-        public static List<ImageColorer> instances = new();
+        public static Dictionary<ConfigDefinition, ZioConfigEntry<Color>> instances = new();
 
         public void Start()
         {
             var panelManager = transform.parent ? GetComponentInParent<PanelManager>() : GetComponent<PanelManager>(); 
             configFile = panelManager ? panelManager.configFile : Load.Instance.configFile;
             image = GetComponent<Image>();
-            configEntry = configFile.Bind(catagory, name, image.color, description);
+            var def = new ConfigDefinition(catagory, name);
+            if (!instances.TryGetValue(def, out configEntry))
+            {
+                configEntry = configFile.Bind(def, image.color, new ConfigDescription(description));
+                instances.Add(def, configEntry);
+                if (Chainloader.PluginInfos.ContainsKey("bubbet.zioriskofoptions"))
+                    MakeRiskOfOptions();
+            }
             configEntry.SettingChanged += SettingChanged;
             SettingChanged(configEntry, default, false);
-            if (instances.Any(x => x.configEntry == configEntry)) return;
-            instances.Add(this);
-            if (Chainloader.PluginInfos.ContainsKey("bubbet.zioriskofoptions"))
-                MakeRiskOfOptions();
+        }
+
+        private void OnDestroy()
+        {
+            configEntry.SettingChanged -= SettingChanged;
         }
 
         private void MakeRiskOfOptions()
         {
             ModSettingsManager.AddOption(new ZioColorOption(configEntry));
-        }
-
-        public void OnDestroy()
-        {
-            if (instances.Contains(this)) instances.Remove(this);
         }
 
         public void SettingChanged(ZioConfigEntryBase entryBase, object oldValue, bool ignoreSave)
