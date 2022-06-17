6b67ab77c61256cf99c3e2bacf2c044004577010
Tyler Souza <tyler6souza@hotmail.com>
2022-06-17 12:34:31 -0400

    up

---
+++ Aerolt/Helpers/SettingsOpener.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Aerolt.Helpers
+{
+    public class SettingsOpener : MonoBehaviour
+    {
+        public void OpenSettingsForced()
+        {
+            
+        }
+        
+    }
+}
