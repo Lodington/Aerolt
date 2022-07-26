e375ed54e7626ff69bd6bcbed07ac1a704028b18
Bubbet <brett1057228@gmail.com>
2022-07-26 09:54:06 -0600

    clean up more shit

--- Aerolt/Managers/BodyManager.cs
+++ Aerolt/Managers/BodyManager.cs
@@ -1,17 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Aerolt.Buttons;
-using Aerolt.Classes;
-using Aerolt.Helpers;
-using Rewired.Utils.Libraries.TinyJson;
 using RoR2;
-using RoR2.ContentManagement;
-using RoR2.UI;
-using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
 
 namespace Aerolt.Managers
 {
@@ -34,23 +23,17 @@ namespace Aerolt.Managers
                 customButton.image.sprite = Sprite.Create((Texture2D)body.portraitIcon, new Rect(0, 0, body.portraitIcon.width, body.portraitIcon.height), new Vector2(0.5f, 0.5f));
                 customButton.button.onClick.AddListener(() => SetBodyDef(body));
             }
-            
         }
 
         public void SpawnAsBody() // TODO network this
         {
-            if (!_newBody)
-            {
-                return;
-            }
-
-            if (target && target.master)
-            {
-                var master = target.master;
-                master.bodyPrefab = _newBody;
-                var transfor = master.GetBody().transform;
-                master.Respawn(transfor.position, transfor.rotation);
-            }
+            if (!_newBody) return;
+            if (!target || !target.master) return;
+            
+            var master = target.master;
+            master.bodyPrefab = _newBody;
+            var transfor = master.GetBody().transform;
+            master.Respawn(transfor.position, transfor.rotation);
         }
 
         public void SetBodyDef(CharacterBody body)
