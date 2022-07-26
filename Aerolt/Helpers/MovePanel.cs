d5734931de50e7c20181fc4677ead9e4e482144e
Tyler Souza <tyler6souza@hotmail.com>
2022-07-25 22:34:45 -0400

    Got Window Resizing and window moving working.

--- Aerolt/Helpers/MovePanel.cs
+++ Aerolt/Helpers/MovePanel.cs
@@ -1,76 +1,29 @@
+using Aerolt.Classes;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using ZioConfigFile;
 
 namespace Aerolt.Helpers
 {
-    public class MovePanel : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+    [RequireComponent(typeof(EventTrigger))]
+    public class MovePanel : MonoBehaviour
     {
-         private Vector2 lastMousePosition;
- 
-    /// <summary>
-    /// This method will be called on the start of the mouse drag
-    /// </summary>
-    /// <param name="eventData">mouse pointer event data</param>
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Debug.Log("Begin Drag");
-        lastMousePosition = eventData.position;
-    }
- 
-    /// <summary>
-    /// This method will be called during the mouse drag
-    /// </summary>
-    /// <param name="eventData">mouse pointer event data</param>
-    public void OnDrag(PointerEventData eventData)
-    {
-        Vector2 currentMousePosition = eventData.position;
-        Vector2 diff = currentMousePosition - lastMousePosition;
-        RectTransform rect = GetComponent<RectTransform>();
- 
-        Vector3 newPosition = rect.position +  new Vector3(diff.x, diff.y, transform.position.z);
-        Vector3 oldPos = rect.position;
-        rect.position = newPosition;
-        if(!IsRectTransformInsideSreen(rect))
+        public GameObject Target;
+        private EventTrigger _eventTrigger;
+        private ZioConfigEntry<Vector2> configEntry;
+        
+        void Start()
         {
-            rect.position = oldPos;
+            _eventTrigger = GetComponent<EventTrigger>();
+            _eventTrigger.AddEventTrigger(OnDrag, EventTriggerType.Drag);
         }
-        lastMousePosition = currentMousePosition;
-    }
- 
-    /// <summary>
-    /// This method will be called at the end of mouse drag
-    /// </summary>
-    /// <param name="eventData"></param>
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("End Drag");
-        //Implement your funtionlity here
-    }
- 
-    /// <summary>
-    /// This methods will check is the rect transform is inside the screen or not
-    /// </summary>
-    /// <param name="rectTransform">Rect Trasform</param>
-    /// <returns></returns>
-    private bool IsRectTransformInsideSreen(RectTransform rectTransform)
-    {
-        bool isInside = false;
-        Vector3[] corners = new Vector3[4];
-        rectTransform.GetWorldCorners(corners);
-        int visibleCorners = 0;
-        Rect rect = new Rect(0,0,Screen.width, Screen.height);
-        foreach(Vector3 corner in corners)
+
+
+        void OnDrag(BaseEventData data)
         {
-            if(rect.Contains(corner))
-            {
-                visibleCorners++;
-            }
+            PointerEventData ped = (PointerEventData) data;
+            
+            Target.transform.Translate(ped.delta);
         }
-        if(visibleCorners == 4)
-        {
-            isInside = true;
-        }
-        return isInside;
-    }
     }
 }
