74bd51fbe6ac10ae73111f10c5de21d4a0a9efe8
Tyler Souza <tyler6souza@hotmail.com>
2022-07-26 18:58:06 -0400

    Added Window saving

--- Aerolt/Helpers/MovePanel.cs
+++ Aerolt/Helpers/MovePanel.cs
@@ -16,13 +16,30 @@ namespace Aerolt.Helpers
         {
             _eventTrigger = GetComponent<EventTrigger>();
             _eventTrigger.AddEventTrigger(OnDrag, EventTriggerType.Drag);
+            _eventTrigger.AddEventTrigger(OnDragEnd, EventTriggerType.EndDrag);
+            
+            var menuInfo = Target.transform.parent ? GetComponentInParent<MenuInfo>() : GetComponent<MenuInfo>();
+            var configFile = menuInfo ? menuInfo.ConfigFile : Load.Instance.configFile;
+            configEntry = configFile.Bind("Window Positions", Target.name, (Vector2) Target.transform.localPosition, "Stored position of this window.");
+            Target.transform.localPosition = new Vector3(configEntry.Value.x, configEntry.Value.y, 0);
+            
         }
 
 
+        void OnDragEnd(BaseEventData data)
+        {
+            if (!Target.transform) return;
+            var targetTransform = (RectTransform) Target.transform;
+            var localPosition = targetTransform.localPosition;
+            var parent = (RectTransform) targetTransform.parent;
+            var width = parent.sizeDelta.x * 0.5f - 10f + targetTransform.sizeDelta.x * 0.5f;
+            var height = parent.sizeDelta.y * 0.5f - 10f + targetTransform.sizeDelta.y * 0.5f;
+            configEntry.Value = new Vector2(Mathf.Clamp(localPosition.x, -width, width), Mathf.Clamp(localPosition.y, -height, height));
+        }
+        
         void OnDrag(BaseEventData data)
         {
             PointerEventData ped = (PointerEventData) data;
-            
             Target.transform.Translate(ped.delta);
         }
     }
