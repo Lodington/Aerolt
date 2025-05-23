7777e2855be205dcf59b1c602bb783262751d422
Bubbet <bubbet@bubbet.dev>
2025-05-23 15:49:26 -0600

    Inital Commit

---
+++ AeroltPlugin.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+
+namespace Aerolt_External;
+
+[BepInPlugin("dev.lodington.aerolt","Aerolt","2.0.0")]
+public class AeroltPlugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        
+    }
+}
