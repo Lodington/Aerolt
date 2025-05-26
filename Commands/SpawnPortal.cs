3373db9737388a59e940a1cd0dc95054a5327916
Bubbet <bubbet@bubbet.dev>
2025-05-26 14:42:40 -0600

    fix spawn portal

---
+++ Commands/SpawnPortal.cs
@@ -0,0 +1,13 @@
+﻿using Aerolt_External.Messages;
+using WebSocketSharp.Net.WebSockets;
+
+namespace Aerolt_External.Commands;
+
+public class SpawnPortal : IWebsocketCommand
+{
+    public string portalType;
+    public void Execute(WebSocketContext context)
+    {
+        new PortalSpawnMessage(portalType).SendToServer();
+    }
+}
