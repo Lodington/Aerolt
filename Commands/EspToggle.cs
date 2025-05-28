f3d710594293ffc97bfafbf9de77d1f4d0a02b64
Lodington <tyler6souza@hotmail.com>
2025-05-27 20:09:25 -0400

    added Teleporter

--- Commands/EspToggle.cs
+++ Commands/EspToggle.cs
@@ -4,18 +4,7 @@ namespace Aerolt_External.Commands;
 
 public class EspToggle : IWebsocketCommand
 {
-    public bool showNewtAlterToggle;
-    public bool showAdvancedToggle;
-    public bool showTeleporterToggle;
-    public bool showChestToggle;
-    public bool showMultiShopToggle;
-    public bool showBarrelToggle;
-    public bool showScrapperToggle;
-    public bool ShowSecretToggle;
-    public bool showDuplicatorToggle;
-    public bool showDroneToggle;
-    public bool showShrineToggle;
-
+    
     public void Execute(WebSocketContext context)
     {
         Esp.ToggleEspOptions(this);
---
+++ Commands/Teleporter.cs
@@ -0,0 +1,32 @@
+﻿using Aerolt_External.Messages;
+using RoR2;
+using WebSocketSharp.Net.WebSockets;
+
+namespace Aerolt_External.Commands;
+
+public class Teleporter : IWebsocketCommand
+{
+    public string portalType;
+    public void Execute(WebSocketContext context)
+    {
+        switch (portalType)
+        {
+            case "spawn_newt_portal":
+            case "spawn_gold_portal":
+            case "spawn_void_portal":
+            case "spawn_celestial_portal":
+            case "spawn_all_portals":
+                new PortalSpawnMessage(portalType.Split("_")[1]).SendToServer();
+                break;
+            case "skip_stage":
+                new SceneChangeMessage().SendToServer();
+                break;
+            case "instant_charge_teleporter":
+                new TeleporterChargeMessage().SendToServer();
+                break;
+            case "add_mountain_shrine":
+                TeleporterInteraction.instance.AddShrineStack();
+                break;
+        }
+    }
+}
